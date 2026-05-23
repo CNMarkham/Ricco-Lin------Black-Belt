ac109fd01771125d45d3fd17dc3fe72a82bb89bc
madelyn <epicrbx2062@gmail.com>
2026-05-23 15:50:40 -0400

    make terrain

---
+++ Working Space Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float distanceFromCamera = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 mousePos = Input.mousePosition;
+        Debug.Log($"Mouse position is {Input.mousePosition}");
+        mousePos.z = distanceFromCamera;
+        Vector3 targetWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        transform.position += Vector3.forward * speed * Time.deltaTime;
+
+        float newX = Mathf.Lerp(transform.position.x, targetWorldPos.x, speed * Time.deltaTime);
+        float newZ = Mathf.Lerp(transform.position.z, targetWorldPos.z, speed * Time.deltaTime);    
+
+        transform.position = new Vector3(newX, transform.position.y, newZ);  
+        /*Vector3 smoothPos = Vector3.Lerp(
+            transform.position,
+            new Vector3(targetWorldPos.x, transform.position.y, targetWorldPos.z),
+            speed * Time.deltaTime
+            );
+
+        transform.position = smoothPos;*/
+    }
+}
