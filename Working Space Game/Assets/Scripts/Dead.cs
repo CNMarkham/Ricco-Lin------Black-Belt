9f8f1030bfb57a8174060ddc48cf38c1647304fd
Eugene <132835392+CNMarkham@users.noreply.github.com>
2026-06-24 17:48:53 -0400

    two player stuff

--- Working Space Game/Assets/Scripts/Dead.cs
+++ Working Space Game/Assets/Scripts/Dead.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Dead : MonoBehaviour
 {
-    public GameObject objectToUnparent;
+
+    public Vector3 newPosition = new Vector3(6.6f, 4.4f, -51.4f);
+    public PlayerMovement playerMovement;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +23,8 @@ public class Dead : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Obstacle"))
         {
-            objectToUnparent.transform.SetParent(null);
-            Destroy(gameObject);
+            transform.position = newPosition;
+            playerMovement.speed = 15;
         }
     }
 }
